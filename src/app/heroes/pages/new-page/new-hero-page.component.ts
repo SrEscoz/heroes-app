ef9dcc8195854d094c6c7d9ce0f396b15c006af4
Miguel Escoz <miguel.escoz@nter.es>
2024-11-04 13:33:49 +0100

    Estructura básica del proyecto

---
+++ src/app/heroes/pages/new-page/new-hero-page.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'app-new-page',
+	templateUrl: './new-hero-page.component.html',
+	styles: ``
+})
+export class NewHeroPageComponent {
+
+}
