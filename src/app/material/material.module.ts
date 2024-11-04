9da191132e334b77bcdee759ef24928e59f3bffd
Miguel Escoz <miguel.escoz@nter.es>
2024-11-04 14:34:33 +0100

    PrimeNG Flex + Angular Material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,39 @@
 import {NgModule} from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatListModule} from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 
 @NgModule({
-	declarations: [],
-	imports: []
+	exports: [
+		MatAutocompleteModule,
+		MatButtonModule,
+		MatCardModule,
+		MatChipsModule,
+		MatDialogModule,
+		MatFormFieldModule,
+		MatGridListModule,
+		MatIconModule,
+		MatInputModule,
+		MatListModule,
+		MatProgressSpinnerModule,
+		MatSelectModule,
+		MatSidenavModule,
+		MatSnackBarModule,
+		MatToolbarModule,
+	]
 })
 export class MaterialModule {
 }
